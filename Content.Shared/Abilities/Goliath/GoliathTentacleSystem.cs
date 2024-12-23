acd64d42263a97814e82c52799bec038bb067116
Koting <jekalavrovih@gmail.com>
2024-12-23 20:08:48 +0300

    Голиаф! Перенесение файлов и исправление багов на Апстриме.

--- Content.Shared/Abilities/Goliath/GoliathTentacleSystem.cs
+++ Content.Shared/Abilities/Goliath/GoliathTentacleSystem.cs
@@ -24,9 +24,52 @@ public sealed class GoliathTentacleSystem : EntitySystem
     public override void Initialize()
     {
         SubscribeLocalEvent<GoliathSummonTentacleAction>(OnSummonAction);
+        SubscribeLocalEvent<GoliathSummonTentacleActionLavaland>(OnSummonActionLavaland);
     }
 
     private void OnSummonAction(GoliathSummonTentacleAction args)
+    {
+        if (args.Handled || args.Coords is not { } coords)
+            return;
+
+        // TODO: animation
+
+        _popup.PopupPredicted(Loc.GetString("tentacle-ability-use-popup", ("entity", args.Performer)), args.Performer, args.Performer, type: PopupType.SmallCaution);
+        _stun.TryStun(args.Performer, TimeSpan.FromSeconds(0.8f), false);
+
+        List<EntityCoordinates> spawnPos = new();
+        spawnPos.Add(coords);
+
+        var dirs = new List<Direction>();
+        dirs.AddRange(args.OffsetDirections);
+
+        for (var i = 0; i < 3; i++)
+        {
+            var dir = _random.PickAndTake(dirs);
+            spawnPos.Add(coords.Offset(dir));
+        }
+
+        if (_transform.GetGrid(coords) is not { } grid || !TryComp<MapGridComponent>(grid, out var gridComp))
+            return;
+
+        foreach (var pos in spawnPos)
+        {
+            if (!_map.TryGetTileRef(grid, gridComp, pos, out var tileRef) ||
+                tileRef.IsSpace() ||
+                _turf.IsTileBlocked(tileRef, CollisionGroup.Impassable))
+            {
+                continue;
+            }
+
+            if (_net.IsServer)
+                Spawn(args.EntityId, pos);
+        }
+
+        args.Handled = true;
+    }
+    // Lavaland Goliath
+
+    private void OnSummonActionLavaland(GoliathSummonTentacleActionLavaland args)
     {
         if (args.Handled || args.Coords is not { } coords)
             return;
